2cc237c2e13fe2d83a01c265bec00ff66aabe217
小傅哥 <184172133@qq.com>
2024-07-27 18:34:07 +0800

    feat: 代码评审

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -1,8 +1,17 @@
 package plus.gaga.middleware.sdk;
 
+import com.alibaba.fastjson2.JSON;
+import plus.gaga.middleware.sdk.domain.model.ChatCompletionSyncResponse;
+import plus.gaga.middleware.sdk.types.utils.BearerTokenUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class OpenAiCodeReview {
 
@@ -26,7 +35,61 @@ public class OpenAiCodeReview {
         int exitCode = process.waitFor();
         System.out.println("Exited with code:" + exitCode);
 
-        System.out.println("评审代码：" + diffCode.toString());
+        System.out.println("diff code：" + diffCode.toString());
+
+        // 2. chatglm 代码评审
+        String log = codeReview(diffCode.toString());
+        System.out.println("code review：" + log);
+    }
+
+    private static String codeReview(String diffCode) throws Exception {
+
+        String apiKeySecret = "c78fbacd3e10118ad5649d7a54a3a163.UunYDBxpzeClvSKZ";
+        String token = BearerTokenUtils.getToken(apiKeySecret);
+
+        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        connection.setDoOutput(true);
+
+        String code = "1+1";
+
+        String jsonInpuString = "{"
+                + "\"model\":\"glm-4-flash\","
+                + "\"messages\": ["
+                + "    {"
+                + "        \"role\": \"user\","
+                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
+                + "    }"
+                + "]"
+                + "}";
+
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+
+        ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
+        return response.getChoices().get(0).getMessage().getContent();
     }
 
 }
