e11cc4a7030744f71dd8597bd5bf78ab26ebcd3a
小傅哥 <184172133@qq.com>
2024-07-27 20:15:25 +0800

    feat: 第6节：评审消息模板通知 v6.1

--- openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("abc9999999"));
+        System.out.println(Integer.parseInt("dddd"));
     }
 
 }
