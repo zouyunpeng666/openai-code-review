5b65382d8dde95af700cba190aff2bb1a9efad32
小傅哥 <184172133@qq.com>
2024-07-27 19:11:27 +0800

    feat: 第5节：评审日志写入文件 v5.1

--- openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/plus/gaga/middleware/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("1234"));
+        System.out.println(Integer.parseInt("abc1234"));
     }
 
 }
