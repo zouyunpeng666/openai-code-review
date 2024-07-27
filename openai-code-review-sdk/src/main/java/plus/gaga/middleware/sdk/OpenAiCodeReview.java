5fb31c84e923a32d90b315457b5075e215bbbe97
小傅哥 <184172133@qq.com>
2024-07-27 18:37:07 +0800

    feat: 代码评审 v4.1

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -86,6 +86,8 @@ public class OpenAiCodeReview {
         in.close();
         connection.disconnect();
 
+        System.out.println("评审结果：" + content.toString());
+
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         return response.getChoices().get(0).getMessage().getContent();
     }
